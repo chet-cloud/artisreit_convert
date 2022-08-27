3c96c153afa67ec2f87c6596f08c7be252ab7207
chet <cchen@artisreit.com>
2022-08-27 12:18:01 -0500

    save

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,61 @@
-use serde_json::{Value, Map, Number};
-use std::{fs, error::Error, result};
+use serde_json::{Map,Value};
+use std::fs;
+use std::fs::{File};
+use std::io;
+use std::path::Path;
+use std::io::prelude::*;
+// https://github.com/dtolnay/serde-yaml
+// https://github.com/serde-rs/json
+// enum Value {
+//     Null,
+//     Bool(bool),
+//     Number(Number),
+//     String(String),
+//     Array(Vec<Value>),
+//     Object(Map<String, Value>),
+// }
+
+fn create_dir_by_array(vec_value:&Vec<Value>,path: &Path){
+    for v in vec_value.iter() {
+        if let Value::String(path_name) = v {
+            let current_path = path.join(path_name);
+            if let Ok(_) = fs::create_dir(current_path) {
+                for v in vec_value.iter(){
+                    match v {
+                        Value::Array(vec) => {
+                            create_dir_by_array(vec,&current_path.clone())
+                        },
+                        Value::Object(map) => {
+                            create_md_by_object(map,&current_path.clone())
+                        },
+                        _ =>{},
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn create_md_by_object(object_value:&Map<String, Value>, path: &Path){
+    if let Some(Value::String(file_name)) =  object_value.get("file_name") {
+        if let Ok(file_content) = serde_json::to_string(&object_value){
+            let current_path = path.join(file_name);
+            echo_with_path(&file_content,&current_path);
+        }
+    }
+}
 
 
-fn json_2_yaml(jsonstr: &str) -> Option<String>{
+pub fn json_create_files(jsonstr: &str,file_path_str:&str){
+    let path = Path::new(file_path_str);
+    if let Ok(Value::Array(vec)) = serde_json::from_str(jsonstr) {
+        create_dir_by_array(&vec, path);
+    }
+}
+
+
+
+pub fn json_to_yaml(jsonstr: &str) -> Option<String>{
     if let Ok(Value::Object(map)) = serde_json::from_str(jsonstr) {
          if let Ok(yaml) = serde_yaml::to_string(&map){
             return Some(yaml)
@@ -12,7 +65,7 @@ fn json_2_yaml(jsonstr: &str) -> Option<String>{
     return None
 }
 
-fn yaml_2_md(yamlstr: &str) -> Option<String>{
+pub fn yaml_to_md(yamlstr: &str) -> Option<String>{
     let mut result = String::new();
     if let Ok(Value::Object(map)) = serde_yaml::from_str(yamlstr) {
          let mut yaml_map = map;
@@ -31,71 +84,57 @@ fn yaml_2_md(yamlstr: &str) -> Option<String>{
     return Some(result);
 }
 
+// A simple implementation of `% cat path`
+fn cat(file_path_str: &str) -> io::Result<String> {
+    let path = Path::new(file_path_str);
+    let mut f = File::open(path)?;
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+// A simple implementation of `% echo s > path`
+fn echo(file_content:&str,file_path_str:&str) -> io::Result<()> {
+    let path = Path::new(file_path_str);
+    let mut f = File::create(path)?;
+    f.write_all(file_content.as_bytes())
+}
+
+fn echo_with_path(file_content:&str,path:&Path) -> io::Result<()> {
+    let mut f = File::create(path)?;
+    f.write_all(file_content.as_bytes())
+}
+
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn test_toMD() {
+    fn test_create_file(){
+        if let Ok(_) = echo("hello world","hello.txt") {
+            assert_eq!(cat("hello.txt").unwrap().to_string(),"hello world");
+            assert!(fs::remove_file("hello.txt").is_ok());
+        }
+    }
+
+    #[test]
+    fn test_to_md() {
         let content = fs::read_to_string("test/test.yaml").unwrap();
-        let result = yaml_2_md(&content).unwrap();
+        let result = yaml_to_md(&content).unwrap();
         let content = fs::read_to_string("test/test.md").unwrap();
         assert_eq!(content,result)
     }
 
-
     #[test]
-    fn test_json_2_yaml() {
+    fn test_json_to_yaml() {
         let content = fs::read_to_string("test/test.json").unwrap();
-        let result = json_2_yaml(&content).unwrap();
+        let result = json_to_yaml(&content).unwrap();
         let content = fs::read_to_string("test/test.yaml").unwrap();
         assert_eq!(content,result)
     }
 
-
-
-    // #[test]
-    fn test_yaml() {
-        // You have some type.
-        let mut map = Map::new();
-        map.insert("x".to_string(), Value::Number(Number::from(1)));
-        map.insert("y".to_string(), Value::Number(Number::from(2)));
-
-        // Serialize it to a YAML string.
-        let yaml = serde_yaml::to_string(&map).unwrap();
-        assert_eq!(yaml, "x: 1\ny: 2\n");
-
-        // Deserialize it back to a Rust type.
-        let deserialized_map: Map<String, Value> = serde_yaml::from_str(&yaml).unwrap();
-        assert_eq!(map, deserialized_map);
-    }
-
-    // #[test]
-    fn test_load_json_file() {
-        // Some JSON input data as a &str. Maybe this comes from the user.
-        let content = fs::read_to_string("test.json").unwrap();
-        // let data = r#"
-        //     {
-        //         "name": "John Doe",
-        //         "age": 43,
-        //         "empty": null,
-        //         "phones": [
-        //             "+44 1234567",
-        //             "+44 2345678"
-        //         ]
-        //     }"#;
-        let data = &content;
-
-        // Parse the string of data into serde_json::Value.
-        let v: Value = serde_json::from_str(data).unwrap();
-
-        // Access parts of the data by indexing with square brackets.
-        println!("Please call {} at the number {}", v["name"], v["phones"][0]);
-
-        assert_eq!(
-            format!("Please call {} at the number {}, is {}", v["name"], v["phones"][0],v["empty"]),
-            "Please call \"John Doe\" at the number \"+44 1234567\", is null"
-        );
-    }
 }
